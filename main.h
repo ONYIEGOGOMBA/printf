ea74aaa2f4221f44b8fa804aa58c012b5c416cc6
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-07-22 10:38:02 -0700

    Task 0

--- main.h
+++ main.h
@@ -7,5 +7,7 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+static void handle_string(va_list args, int *printed_chars);
+static void handle_char(va_list args, int *printed_chars);
 
 #endif
